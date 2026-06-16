15097891e1b4c0fef563e98f9ab78d2c8162548f
AF <alexandro.frias2@gmail.com>
2026-06-16 15:23:26 -0700

    add dim adjuster and policy

---
+++ claims-analytics/dbt_claims/models/silver/dim_adjuster.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table', schema='silver' )}}
+
+SELECT HASH(
+        adjuster_id
+        ,adjuster_name
+        ,adjuster_region
+        ,adjuster_experience_years
+        ) AS adjuster_sk
+    ,adjuster_id
+    ,adjuster_name
+    ,adjuster_region
+    ,adjuster_experience_years
+FROM {{ ref('stg_claims_extract') }}
+QUALIFY ROW_NUMBER() OVER 
+    (PARTITION BY adjuster_id
+    ORDER BY stg_loaded_at DESC) = 1
---
+++ claims-analytics/dbt_claims/models/silver/dim_policy.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table', schema='silver' )}}
+
+SELECT HASH(
+        policy_id
+        ,policy_product_type
+        ,policy_state
+        ,policy_effective_date
+        ,policy_expiration_date
+        ) AS policy_sk
+    ,policy_id
+    ,policy_product_type
+    ,policy_state
+    ,policy_effective_date
+    ,policy_expiration_date
+FROM {{ ref('stg_claims_extract') }}
+QUALIFY ROW_NUMBER() OVER 
+    (PARTITION BY policy_id
+    ORDER BY stg_loaded_at DESC) = 1
